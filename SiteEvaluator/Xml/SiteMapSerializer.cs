83f97683895033903f0b3accf9d4432c0975dd40
Alex Korneiko <a.korneyko@gmail.com>
2022-03-10 08:00:33 +0200

    BugFix: Using dynamic namespaces for xml serializer

--- SiteEvaluator/Xml/SiteMapSerializer.cs
+++ SiteEvaluator/Xml/SiteMapSerializer.cs
@@ -13,7 +13,9 @@ namespace SiteEvaluator.Xml
 
         public static SiteMap Deserialize(string xmlString)
         {
-            var xmlSerializer = new XmlSerializer(typeof(SiteMap));
+            var namespaceForUrlset = GetNamespaceForUrlset(xmlString);
+            
+            var xmlSerializer = new XmlSerializer(typeof(SiteMap), namespaceForUrlset);
 
             using var stream = GetStream(xmlString);
 
@@ -36,6 +38,26 @@ namespace SiteEvaluator.Xml
             streamWriter.Flush();
             memoryStream.Position = 0;
             return memoryStream;
+        }
+
+        private static string GetNamespaceForUrlset(string xmlString)
+        {
+            if (string.IsNullOrEmpty(xmlString)) return "";
+
+            const string urlsetXmlns = "urlset xmlns=\"";
+            
+            var indexOfStartUrlsetXmlnsName = xmlString
+                .IndexOf(urlsetXmlns, StringComparison.InvariantCulture) + urlsetXmlns.Length;
+
+            var lengthUrlsetXmlnsName = xmlString
+                .IndexOf('\"', indexOfStartUrlsetXmlnsName) - indexOfStartUrlsetXmlnsName;
+
+            if (indexOfStartUrlsetXmlnsName > -1 && lengthUrlsetXmlnsName > 0)
+            {
+                return xmlString.Substring(indexOfStartUrlsetXmlnsName, lengthUrlsetXmlnsName);
+            }
+
+            return "";
         }
     }
 }
