cf3a4afdfc078f99bd431c1d43916d1f7338c05e
Alex Korneiko <a.korneyko@gmail.com>
2022-03-31 11:36:08 +0300

    ConsoleUI Microsoft Dependency Injection

---
+++ SiteEvaluator.ConsoleUI.MsHosting/HostedApplication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace SiteEvaluator.ConsoleUI.MsHosting
+{
+    public class HostedApplication : 
+        Application,
+        IHostedService
+    {
+        public HostedApplication(IConsoleView consoleView)
+            : base(consoleView, Environment.GetCommandLineArgs()[1..])
+        {
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await base.StartAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SiteEvaluator.ConsoleUI.MsHosting/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SiteEvaluator.Crawler;
+using SiteEvaluator.Data;
+using SiteEvaluator.DataLoader;
+using SiteEvaluator.DataLoader.HttpLoader;
+using SiteEvaluator.Html;
+using SiteEvaluator.Presentation;
+using SiteEvaluator.SiteMapExploring;
+using SiteEvaluator.SiteMapExploring.Parser;
+
+namespace SiteEvaluator.ConsoleUI.MsHosting
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            return CreateBuilder(args)
+                .Build()
+                .RunAsync();
+                // .BuildWithStartup<ConsoleView>();
+        }
+
+        private static IHostBuilder CreateBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    //Services. Zero layer
+                    services.AddScoped<IHttpLoaderService, HttpLoaderService>();
+
+                    //Services. First layer
+                    services.AddScoped<IContentLoaderService, ContentLoaderService>();
+                    services.AddScoped<IHtmlParseService, HtmlParseService>();
+                    services.AddScoped<ISiteMapParseService, SiteMapParseService>();
+                    services.AddScoped<IDao<PageInfo>, FileDao<PageInfo>>();
+
+                    //Services. Second layer
+                    services.AddScoped<ISiteCrawler, SiteCrawler>();
+                    services.AddScoped<ISiteMapExplorer, SiteMapExplorer>();
+                    services.AddScoped<IReportService, ReportService>();
+
+                    services.AddScoped<IConsoleView, ConsoleView>();
+
+                    services.AddHostedService<HostedApplication>();
+                });
+        }
+    }
+}
